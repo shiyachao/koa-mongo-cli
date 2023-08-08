18a1b97610ae4fc5ec4343d8428ac7744ac130e4
Shi Yachao <yachao.shi@innocellence.com>
2023-08-08 11:08:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# koa-mongo-cli
+
+## Installation
+
+```sh
+$ npm i koa-mongo-cli -g
+```
+
+create project
+
+```bash
+$ koa-mongo-cli create <your project name>
+```
+
+[source code](https://github.com/shiyachao/koa-init)
---
+++ bin/index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const program = require("commander");
+const download = require("download-git-repo");
+const handlebars = require("handlebars");
+const fs = require("fs");
+
+program
+  .command("create <project>")
+  .description("create a koa project")
+  .action((projectName) => {
+    const url = "github:shiyachao/koa-init#cli";
+    console.log("loading template...");
+    download(url, projectName, { clone: true }, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("template dowload successfully");
+        const packagePath = `${projectName}/package.json`;
+        const packageContent = fs.readFileSync(packagePath, "utf-8");
+        const packageRepResult = handlebars.compile(packageContent)({
+          name: projectName,
+        });
+        fs.writeFileSync(packagePath, packageRepResult);
+        console.log("Project creation completed");
+      }
+    });
+  });
+program.parse(process.argv);
